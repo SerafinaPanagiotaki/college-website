799971d18b9ab1f7b0c52490493de165696d938f
Serafina Panagiotaki <std167437@ac.eap.gr>
2024-08-18 12:34:34 +0300

    Add files via upload

---
+++ iekaigaleo.js
@@ -0,0 +1,8 @@
+function info(){
+    alert("Η σελίδα κατασκευάστηκε με χρήση HTML5, CSS και JavaScript.\n\nΣέρη Παναγιωτάκη - 2ετής σπουδάστρια ΤΕΠ Δ.ΙΕΚ ΔΥΠΑ Αιγάλεω - Project HTML Γ' Εξαμήνου 2023");
+}
+
+function welcome(){
+    //location="iekaigaleo.html";
+    alert("Καλώς ήρθατε στην επίσημη ιστοσελίδα του Δ.ΙΕΚ ΔΥΠΑ Αιγάλεω!");
+}
